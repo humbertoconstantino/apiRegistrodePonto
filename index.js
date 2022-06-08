a7a785efb749bb8af85a4460477fc74646c9f331
Humberto <humbertoconstantino73@gmail.com>
2022-06-08 00:51:31 -0300

    env

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ app.get('/', (req, res) =>{
     res.send(this.register)
     this.register = false
 }) 
-
+const port = process.env.PORT || 3333
 app.listen(3333, (console.log('Servidor aberto')))
